dce5f7833a718f9236a882414c8e2821f2c958cd
MEHLMAN <mailedouardmehlman@gmail.com>
2016-07-21 11:12:09 -0700

    Exploring the instanced extension

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ var getImages = function(list){
 
 var main=function(data) {
 
+  var useInstantiation = true;
+
   canvas=document.getElementById("canvas_webgl");
   container = document.createElement( 'div' );
   document.body.appendChild( container );
@@ -41,6 +43,7 @@ var main=function(data) {
     return false;
   }
 
+  GLext = GL.getExtension("ANGLE_instanced_arrays");
   GL.getExtension("OES_element_index_uint");
 
   GL.disable(GL.DEPTH_TEST);
@@ -58,41 +61,44 @@ var main=function(data) {
     console.log("OK")
     return data_tmp
   }
-  console.log(moveDataJellyfish(0,1,1));
 
-  var jellyfish_army = data.army_coordinates.map((coord)=>{
-    var data_tmp = moveDataJellyfish(coord[0],coord[1],coord[2]);
-    data_tmp.jellyfish.images = data.jellyfish.images
-    return new Jellyfish(GL,data_tmp);
-  });
-  //var data = JSON.parse(JSON.stringify(data))
-  //console.log(data);
-  //console.log(data);
+  if(!useInstantiation){
+      var jellyfish_army = data.army_coordinates.map((coord)=>{
+      var data_tmp = moveDataJellyfish(coord[0],coord[1],coord[2]);
+      data_tmp.jellyfish.images = data.jellyfish.images
+      return new Jellyfish(GL,data_tmp);
+    });
+  }else{
+      var instancedJellyfish = new InstancedJellyfish(GL,GLext,data);
+  }
 
 
-
-  // data.jellyfish.vertices = data.jellyfish.vertices.map((coord,i)=>{
-  //   return ((i%3)==0)?(coord+6):coord;
-  // })
-  //var jellyfish = new Jellyfish(GL,data);
-  var gradient = new Gradient(GL,data.gradient);
+  //var gradient = new Gradient(GL,data.gradient);
 
   var drawing = function(){
     GL.viewport(0, 0, canvas.width, canvas.height);
     GL.clearColor(1.,1.,1.,1.);
-    gradient.render();
-    jellyfish_army.map((jellyfish)=>{
-      jellyfish.render();
-    })
+    //gradient.render();
+    if(!useInstantiation){
+      jellyfish_army.map((jellyfish)=>{
+        jellyfish.render();
+      })
+    }else{
+      instancedJellyfish.render();
+    }
 
   }
 
   function onResize () {
     canvas.width = canvas.clientWidth
     canvas.height = canvas.clientHeight;
-    jellyfish_army.map((jellyfish)=>{
-      jellyfish.updateViewport(canvas);
-    })
+    if(!useInstantiation){
+      jellyfish_army.map((jellyfish)=>{
+        jellyfish.updateViewport(canvas);
+      })
+    }else{
+      instancedJellyfish.updateViewport(canvas);
+    }
   }
   window.addEventListener("resize", onResize, false);
   onResize();
@@ -109,14 +115,14 @@ var main=function(data) {
   animate(0);
 }
 
-var promise_shader_vertex_source = getXHR('./shaders/jellyfish.vert')
+var promise_shader_vertex_source = getXHR('./shaders/instanced_jellyfish.vert')
 var promise_shader_fragment_source = getXHR('./shaders/jellyfish.frag')
 
-var jellyfish_vertices = getXHR('./data/jellyfish_vertices.json').then(JSON.parse)
-var jellyfish_normals = getXHR('./data/jellyfish_normals.json').then(JSON.parse)
-var jellyfish_texture = getXHR('./data/jellyfish_texture.json').then(JSON.parse)
-var jellyfish_colors = getXHR('./data/jellyfish_colors.json').then(JSON.parse)
-var jellyfish_ifaces = getXHR('./data/jellyfish_ifaces.json').then(JSON.parse)
+var jellyfish_vertices = getXHR('./data/attributes/jellyfish_vertices.json').then(JSON.parse)
+var jellyfish_normals = getXHR('./data/attributes/jellyfish_normals.json').then(JSON.parse)
+var jellyfish_texture = getXHR('./data/attributes/jellyfish_texture.json').then(JSON.parse)
+var jellyfish_colors = getXHR('./data/attributes/jellyfish_colors.json').then(JSON.parse)
+var jellyfish_ifaces = getXHR('./data/attributes/jellyfish_ifaces.json').then(JSON.parse)
 
 var jellyfish_list = getXHR('./data/img/list.json').then(JSON.parse).then(getImages)
 
@@ -133,7 +139,7 @@ var array_promise = [
   getXHR('./shaders/gradient.frag'),
   getXHR('./shaders/debug.vert'),
   getXHR('./shaders/debug.frag'),
-  getXHR('./data/jellyfish_army_coordinates.json').then(JSON.parse)
+  getXHR('./data/group/jellyfish_army_coordinates_full.json').then(JSON.parse)
 ];
 
 Promise.all(array_promise).then(function(values){
@@ -141,7 +147,7 @@ Promise.all(array_promise).then(function(values){
       shaders:{VS:values[0],FS:values[1]},
       gradient:{VS:values[8],FS:values[9]},
       debug:{VS:values[10],FS:values[11]},
-      jellyfish:{vertices:values[2],normals:values[3],texture:values[4],colors:values[5],faces:values[6],images:values[7]},
+      jellyfish:{vertices:values[2],normals:values[3],texture:values[4],colors:values[5],faces:values[6],images:values[7],offset: values[12]},
       army_coordinates: values[12]
     });
 });
