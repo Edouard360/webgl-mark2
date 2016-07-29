4b0ea046263b7352dab76cf9639e89aac31984bd
Baptiste Manson <baptiste.manson@inovia.fr>
2016-07-29 15:11:03 -0700

    Embedded server was not added in the package json

--- bin/server-start.js
+++ bin/server-start.js
@@ -1,5 +1,13 @@
 var tmp = console.log
-console.log = function() {}
-require('simple-server')
-console.log = tmp
-console.log('To see the benchmark, please open your browser at http://localhost:3000/index.html')
+console.log = function () {}
+
+try {
+  require('simple-server')
+  console.log = tmp
+  console.log('To see the benchmark, please open your browser at http://localhost:3000/index.html')
+} catch (err) {
+  console.log = tmp
+  console.log('We couldn\'t start the server, probably the address http://localhost:3000 is already in use?')
+  console.log(err.message)
+}
+
